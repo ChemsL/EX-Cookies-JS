4ceefaef5f8e9867d768389a38cfdc62ad909d90
unknown <chems.ly@hotmail.fr>
2023-10-23 13:25:49 +0200

    commit2

---
+++ script.js
@@ -0,0 +1,125 @@
+class humain {
+    constructor(id, pseudo, picture, age, gender, search, region, description){
+    this.id = id
+    this.pseudo = pseudo
+    this.picture = picture
+    this.age = age
+    this.gender = gender
+    this.search = search
+    this.region = region
+    this.description = description
+}
+}
+
+const carole = new humain (
+    "Carole",
+    "La ptite Caro",
+    "carole.jfif",
+    "51 ans",
+    "Femme",
+    "Homme",
+    "Franche-Compté",
+    "Passionée de rando, j'ai également un penchant our les petit jeunes ;)"
+)
+
+const dani = new humain (
+    "Daniel",
+    "Dani",
+    "dani.jfif",
+    "46 ans",
+    "Homme",
+    "Femme",
+    "Midi-Pyrénnée",
+    "Salut, moi c'est Dani, fan de pétanque et de pastis !"
+)
+const gilbert = new humain (
+    "Gilbert",
+    "Gigi",
+    "gigi.jfif",
+    "42 ans",
+    "Homme",
+    "Femme",
+    "Ile-de-France",
+    "Bonjour, je suis Gilbert, passioné de musique classique, je suis malvoyant."
+)
+const Hughes = new humain (
+    "Hugues",
+    "Hughes",
+    "hugue.jfif",
+    "38 ans",
+    "Homme",
+    "Femme",
+    "Ile-de-France",
+    "Hello world ! Je m'appellen Hugues, je suis développeur."
+)
+const Jeanne = new humain (
+    "Jeanne",
+    "Jeanneette78",
+    "jeanne.jfif",
+    "35 ans",
+    "Femme",
+    "Homme",
+    "Ile-de-France",
+    "Bonjour, je suis Jeanne, pétillante, je n'ai qu'une passion : les chats !"
+)
+const Bertrand = new humain (
+    "Bertrand",
+    "Beber",
+    "bertrand.jfif",
+    "50 ans",
+    "Homme",
+    "Femme",
+    "Nord Pas de Calais",
+    "Bonjour, je suis Bertrand, entrepreneur d'une PME, et fan de finances."
+)
+const Bruno = new humain (
+    "Bruno",
+    "Kizaru",
+    "bruno.jfif",
+    "48 ans",
+    "Homme",
+    "Femme",
+    "Seine-Maritime",
+    "Bonjour, je suis Bruno j'adore les animés, de jeux vidéos, et de brocantes !"
+)
+const christine = new humain (
+    "Christine",
+    "Cricri",
+    "christine.jfif",
+    "44 ans",
+    "Femme",
+    "Homme et femme",
+    "Bretagne",
+    "Hello, moi c'est Christine, en harmonie avec la nature, 100% écolo, j'aime le terroir !",
+)
+const eric = new humain (
+    "Eric",
+    "Rick",
+    "eric.jfif",
+    "40 ans",
+    "Homme",
+    "Femme",
+    "Picardie",
+    "Bonjour, enchanté je suis Eric, peace & love.",
+)
+const abdallah = new humain (
+    "Abdallah",
+    "Abd",
+    "abdallah.jfif",
+    "42 ans",
+    "Homme",
+    "Femme",
+    "Morbihan",
+    "Bonjour, je suis Abdallah, ingénieur en aéronotique.",
+)
+const bernadette = new humain (
+    "Bernadette",
+    "Nenette",
+    "bernadette.jfif",
+    "59 ans",
+    "Femme",
+    "Homme",
+    "Auvergne",
+    "Hey, je suis Bernadette mais tu peux m'appeler Nenette. Je kiffe netflix and chill :).",
+)
+
